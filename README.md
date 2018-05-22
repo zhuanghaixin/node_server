80d5a90eb2ae0f6ce20403166f036564c4f7fb29
zhuanghaixin <649308293@qq.com>
2018-05-22 20:55:24 +0800

    添加 image发送请求 script发送请求 jsonp发送请求 jquery中的jsonp发送请求

--- README.md
+++ README.md
@@ -15,4 +15,23 @@ node server1_1.js 8888
 ```
 node server1_2.js 8888
 ```
-## 3.用户付款案例(使用文件系统数据库）
+## 3.用户付款案例(使用文件系统数据库并用iframe进行优化）
+```
+node server1_3.js 8888
+```
+## 4.用户付款案例(使用文件系统数据库并用img标签发送请求)
+```
+nodenode server1_4.js 8888
+```
+## 5.用户付款案例(使用文件系统数据库并用script标签发送请求，然后在服务器端（刷新页面或者修改innerText)
+```
+nodenode server1_5.js 8888
+``
+## 6.用户付款案例(使用文件系统数据库并用script标签发送请求，比如说是frank写的前端页面向jack写的后台发送请求) **JSONP**
+```
+nodenode server1_6.js 8888
+```
+## 7.用户付款案例（jQuery中的jsonp)
+```
+nodenode server1_7.js 8888
+```
---
+++ server1_5.js
@@ -0,0 +1,84 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var path = request.url
+    var query = ''
+    if (path.indexOf('?') >= 0) {
+        query = path.substring(path.indexOf('?'))
+    }
+    var pathNoQuery = parsedUrl.pathname
+    var queryObject = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log('方方说：得到 HTTP 路径为\n' + path)
+    console.log('方方说：查询字符串为\n' + query)
+    console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+    if (path == '/') {
+        var string = fs.readFileSync('./index1_3.html','utf8');
+        var amount=fs.readFileSync('./db','utf8'); //100
+        string=string.replace('&&&amount&&&',amount);
+        response.setHeader('Content-Type', 'text/html; charset=utf-8')
+        response.write(string);
+        response.end()
+    }else if(path=='/pay'){
+        var amount=fs.readFileSync('./db','utf8') //100
+        var newAmount=amount-1;
+        if(Math.random()>0.5){
+            fs.writeFileSync('./db',newAmount);
+            response.setHeader('Content-Type','application/javascript');
+            response.statusCode=200;
+            response.write(`alert("success");
+            amount.innerText=amount.innerText-1
+            
+            `);
+
+        }else{
+            // response.statusCode=400;
+            response.write('alert("失败")');
+        }
+        response.end();
+
+    }
+    else if (path == '/style1&style2.css') {
+        response.setHeader('Content-Type', 'text/css; charset=utf-8')
+        response.write('body{background-color: #ddd;}h1{color: red;}')
+        response.end()
+    } else if (path == '/main.js') {
+        response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+        response.write('alert("这是JS执行的")')
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.end()
+    }
+
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
