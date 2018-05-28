f040498cc9fcbe718d29a874feb600f7d2de46bc
zhuanghaixin <649308293@qq.com>
2018-05-28 22:25:28 +0800

    只有协议+端口+域名一模一样，才可以发送Ajax请求

--- README.md
+++ README.md
@@ -21,18 +21,25 @@ node server1_3.js 8888
 ```
 ## 4.用户付款案例(使用文件系统数据库并用img标签发送请求)
 ```
-nodenode server1_4.js 8888
+node server1_4.js 8888
 ```
 ## 5.用户付款案例(使用文件系统数据库并用script标签发送请求，然后在服务器端（刷新页面或者修改innerText)
 ```
-nodenode server1_5.js 8888
+node server1_5.js 8888
 ```
 ## 6.用户付款案例(使用文件系统数据库并用script标签发送请求，比如说是frank写的前端页面向jack写的后台发送请求) **JSONP**
 ```
-nodenode server1_6.js 8888
+node server1_6.js 8888
 ```
 ## 7.用户付款案例（jQuery中的jsonp)
 ```
-nodenode server1_7.js 8888
+node server1_7.js 8888
 ```
-## 8.AJAX
+## 8.使用原生JS写的AJAX来发送请求
+```
+node server2.js 8888
+```
+## 9.只有 协议+端口+域名 一模一样才允许发 AJAX 请求,form表单可以跨源发送请求
+```
+node server3.js 8888
+```
---
+++ server3.js
@@ -0,0 +1,94 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    process.exit(1)
+}
+
+var server = http.createServer(function (request, response) {
+    var parsedUrl = url.parse(request.url, true)
+    var path = request.url
+    var query = ''
+    if (path.indexOf('?') >= 0) {
+        query = path.substring(path.indexOf('?'))
+    }
+    var pathNoQuery = parsedUrl.pathname
+    var queryObject = parsedUrl.query
+    var method = request.method
+
+    /******** 从这里开始看，上面不要看 ************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+    console.log('方方说：得到 HTTP 路径为\n' + path)
+    console.log('方方说：查询字符串为\n' + query)
+    console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+    if (path == '/') {
+        var string = fs.readFileSync('./index3.html');
+        response.setHeader('Content-Type', 'text/html; charset=utf-8')
+        response.write(string)
+        response.end()
+    }
+    else if (path == '/style1&style2.css') {
+        response.setHeader('Content-Type', 'text/css; charset=utf-8')
+        response.write('body{background-color: #ddd;}h1{color: red;}')
+        response.end()
+    } else if (path == '/main.js') {
+        var string = fs.readFileSync('./main.js')
+        response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+        response.write(string)
+        response.end()
+    }  else if (path == '/main2.js') {
+        var string = fs.readFileSync('./main2.js')
+        response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+        response.write(string)
+        response.end()
+    } else if (path == '/xxx') {
+        response.statusCode = 200;
+        // response.setHeader('Content-Type', 'text/xml; charset=utf-8')
+        response.setHeader('Content-Type', 'text/json; charset=utf-8')
+
+
+//         response.write(`<?xml version="1.0" encoding="UTF-8"?>
+// <note>
+//   <to>芳芳</to>
+//   <from>小姑</from>
+//   <heading>你好</heading>
+//   <body>Don't forget me this weekend!</body>
+// </note>`)
+        response.write(`
+        {
+        "note":{
+            "to":"小姑",
+            "from":"芳芳",
+            "heading":"你好",
+            "body":"你好世界"
+            } 
+       }
+        `);
+        response.end()
+    } else {
+        response.statusCode = 404
+        response.end()
+    }
+
+
+    /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
