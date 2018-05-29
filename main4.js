9d6ad5996196bb3fc97400cb2620e61dcff3ee1d
zhuanghaixin <649308293@qq.com>
2018-05-29 13:07:38 +0800

    设置headers

--- main4.js
+++ main4.js
@@ -15,10 +15,15 @@ window.jQuery.ajax = function (options) {
     let url=options.url;
     let method=options.method;
     let body=options.body;
+    let headers=options.headers;
     let successFn=options.successFn;
     let failFn=options.failFn;
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
+    for(let key in headers){
+        let value=headers[key];
+        xhr.setRequestHeader(key,value);
+    }
     xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
@@ -48,6 +53,10 @@ btn.addEventListener('click', (e) => {
         'url':'/xxx',
         'method':'post',
         'body':'a=1&b=2',
+        'headers':{
+            'Content-Type':'application/x-www-form-urlencoded',
+            'name':'frank'
+        },
         'successFn':(e)=>{
             f1.call(undefined,e);
             f2.call(undefined,e);
