c4e486ca467594c6b80a613e2da16304b83c248a
zhuanghaixin <649308293@qq.com>
2018-05-29 22:00:51 +0800

    Promise Ajax

--- main6.js
+++ main6.js
@@ -10,8 +10,15 @@ window.jQuery=function(nodeOrSelector){
 
 
 }
-
-window.jQuery.ajax = function ({url,method,body,headers,successFn,failFn}) {
+// window.Promise=function(fn){
+//     //....
+//     return {
+//         then:function () {
+//            
+//         }
+//     }
+// }
+window.jQuery.ajax = function ({url,method,body,headers}) {
     //一个函数如何接受两种参数
     // let url
     //
@@ -29,34 +36,28 @@ window.jQuery.ajax = function ({url,method,body,headers,successFn,failFn}) {
     //ES6解构
     // let{url,method,body,headers,successFn,failFn}=options;
 
-
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    for(let key in headers){
-        let value=headers[key];
-        xhr.setRequestHeader(key,value);
-    }
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                successFn.call(undefined,xhr.responseText);
-            } else if (xhr.status >= 400) {
-                failFn.call(undefined,xhr);
+    return new Promise(function (resolve,reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        for(let key in headers){
+            let value=headers[key];
+            xhr.setRequestHeader(key,value);
+        }
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve.call(undefined,xhr.responseText);
+                } else if (xhr.status >= 400) {
+                    reject.call(undefined,xhr);
+                }
             }
         }
-    }
+        xhr.send(body);
+    })
 
 
-    xhr.send(body);
+}
 
-}
-//执行成功后，调用两个函数
-function f1(responseText){
-    console.log(1);
-}
-function f2(responseText){
-    console.log(2);
-}
 
 
 
@@ -68,15 +69,15 @@ btn.addEventListener('click', (e) => {
         'headers':{
             'Content-Type':'application/x-www-form-urlencoded',
             'name':'frank'
-        },
-        'successFn':(e)=>{
-            f1.call(undefined,e);
-            f2.call(undefined,e);
-        },
-        'failFn':(xhr)=>{console.log(xhr.status);
-            console.log(xhr.responseText);
         }
 
+
     }
-    window.jQuery.ajax(obj);
+    window.jQuery.ajax(obj)
+        .then(
+            (responseText)=>{
+        console.log(responseText);},
+            (request)=>{
+        console.log(request)}
+        )
 })
